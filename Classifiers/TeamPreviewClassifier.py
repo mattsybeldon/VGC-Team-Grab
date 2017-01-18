500df6842d204cd68c03e5a3c9a44cafebcaa9f4
Matt Sybeldon <mattsybeldon@gmail.com>
2017-01-18 12:36:09 -0500

    Defunct now since we aren't using corner detection

--- Classifiers/TeamPreviewClassifier.py
+++ Classifiers/TeamPreviewClassifier.py
@@ -1,4 +1,5 @@
 '''
+NOTE: DEFUNCT. NO LONGER USING CORNER DETECTION. SEE TrainTeamPreview.py which is what is being used atm
 Team Previewer Classifier
 Written by: Matt Sybeldon (bearsfan092)
 
@@ -6,11 +7,13 @@ This classifier is the first stage in a multi-stage process. In this part, the t
 This is so we don't scan meaningless screens such as screen transitions or battles (although I might add battle support
 later... would be interesting to see the subselections of 4, but not even Showdown provides that info).
 
-This is a relatively simple problem since the classes are Team Preview vs. not Team Preview. For now, I'm trying a SVM
-classifier using corners as features. Those level markers should work pretty nice for that purpose since they're regular
+This is a relatively simple problem since the classes are Team Preview vs. not Team Preview.
+Definitely want to use template matching since the bottom box is mostly invariant. Then we can use a linear SVM
+classifier.
 '''
 
-from Features import cornerORB
+import numpy as np
+from Features import TemplateMatch
 from os import listdir
 from os.path import isfile, join
 import cv2
@@ -19,11 +22,16 @@ import cv2
 positiveFiles = [f for f in listdir('../Training/TeamPreview/Positive') if isfile(join('../Training/TeamPreview/Positive', f))]
 negativeFiles = [f for f in listdir('../Training/TeamPreview/Negative') if isfile(join('../Training/TeamPreview/Negative', f))]
 
-for i in positiveFiles:
-    positiveFeatures = cornerORB.returnCornersORB('../Training/TeamPreview/Positive/' + i)
+#Initialize scores
+positiveScores = np.empty([len(positiveFiles), 1], dtype = float)
+negativeScores = np.empty([len(negativeFiles), 1], dtype = float)
 
-for j in negativeFiles:
-    negativeFeatures = cornerORB.returnCornersORB('../Training/TeamPreview/Negative/' + j)
+
+for i in xrange(0, len(positiveFiles) - 1):
+    positiveScores[i] = cornerORB.returnCornersORB('../Training/TeamPreview/Positive/' + i)
+
+for i in xrange(0, len(positiveFiles) - 1):
+    negativeScores[i[]] = cornerORB.returnCornersORB('../Training/TeamPreview/Negative/' + j)
 
 
 
