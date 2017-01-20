9cd052b2d2c50b65a5cc0b4a82578180ac80099b
Matt Sybeldon <mattsybeldon@gmail.com>
2017-01-20 10:24:06 -0500

    Barely did anything with this but starting to work on the main flow.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import cv2
 import numpy as np
 from tasks import grab_screen
+from tasks import screenset
 from Preprocessing import resize_top_screen
 
-img = grab_screen.return_screen_grab(0,72,625,447) #We'll make this better later.
 
-img_resize = resize_top_screen.resize_3ds(img, 400)
+print('Waiting for screen selection region. Click upper left corner and drag to lower right.')
 
-cv2.imshow('Screen', img_resize)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+capture_coords = screenset.configure_screen()
+left = capture_coords[0][0]
+upper = capture_coords[0]
+
+print(capture_coords)
+
+#Body loop starts here
+while True:
+
+    grab_screen.return_screen_grab()
+
+
+# img_resize = resize_top_screen.resize_3ds(img, 400, 240)
+#
+# cv2.imshow('Screen', img_resize)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
